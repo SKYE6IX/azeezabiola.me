9c95e354dc1ea3d8eb2bc94c585a337ae903a2a3
SKYE <skye@MacBook-Pro-MacBook.local>
2024-01-18 18:38:08 +0300

    Added active indicator to nav bar

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+const route = useRoute();
 const navs = [
    { id: "1", name: "_hello", href: "/" },
    { id: "2", name: "_about-me", href: "/about" },
    { id: "3", name: "_projects", href: "/project" },
-   { id: "4", name: "_contact-me", href: "/contact" },
+   { id: "4", name: "_contact-me", href: "/contact" }
 ];
 </script>
 
@@ -11,8 +12,13 @@ const navs = [
    <header class="header">
       <h5 class="header__title">azeez-abiola</h5>
       <nav>
-         <ul class="header__nav-list">
-            <li v-for="nav in navs" :key="nav.id">
+         <ul class="nav-lists">
+            <li
+               v-for="nav in navs"
+               :key="nav.id"
+               class="nav-list"
+               :class="{ active: nav.href === route.fullPath }"
+            >
                <NuxtLink :to="nav.href">{{ nav.name }}</NuxtLink>
             </li>
          </ul>
