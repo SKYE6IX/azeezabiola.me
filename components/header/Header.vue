542b8353a1989a3fcc48f978f40df274e7785ba3
SKYE <skye@MacBook-Pro-MacBook.local>
2024-05-15 14:03:28 +0300

    #

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -29,7 +29,16 @@ onMounted(() => {
 <template>
    <header class="header">
       <h5 class="header__title">azeez-abiola</h5>
-      <nav class="header__navigation">
+      <div
+         class="header__menu-bar"
+         :class="{ open: isOpen, close: !isOpen }"
+         @click="setIsOpen"
+      >
+         <div class="bar"></div>
+         <div class="bar"></div>
+         <div class="bar"></div>
+      </div>
+      <nav class="header__navigation" :class="{ open: isOpen, close: !isOpen }">
          <ul class="nav-lists" :style="navItemVariables">
             <li
                v-for="nav in navs"
@@ -41,41 +50,15 @@ onMounted(() => {
                      navRefs.push(el);
                   }
                "
+               @click="setIsOpen"
             >
                <NuxtLink :to="nav.href">{{ nav.name }}</NuxtLink>
             </li>
          </ul>
+         <Footer />
       </nav>
-      <!-- Menu Bar -->
-      <!-- <div
-         class="header__menu-bar"
-         :class="{ 'menu-open': isOpen, 'menu-close': !isOpen }"
-         @click="setIsOpen"
-      >
-         <div class="bar"></div>
-         <div class="bar"></div>
-         <div class="bar"></div>
-      </div> -->
    </header>
-   <!-- Mobile Naviagtion Container -->
-   <!-- <div class="header__menu-slide-in" :class="{ open: isOpen, close: !isOpen }">
-      Mobile Navigation
-      <ul class="nav-lists">
-         <li
-            v-for="nav in navs"
-            :key="nav.id"
-            class="nav-list"
-            :class="{ active: nav.href === route.fullPath }"
-            @click="setIsOpen"
-         >
-            <NuxtLink :to="nav.href">{{ nav.name }}</NuxtLink>
-         </li>
-      </ul>
-      Mobile Footer
-      <Footer />
-   </div> -->
 </template>
-
 <style scoped lang="scss">
 @import "./assets/header.scss";
 </style>
