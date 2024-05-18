1649c2cff11f5d65185493c5cec9ef9495bd5bbc
SKYE <skye@192.168.1.5>
2024-05-19 00:25:39 +0300

    #

--- components/project/ProjectNavs.vue
+++ components/project/ProjectNavs.vue
@@ -12,52 +12,57 @@ defineProps<Props>();
 </script>
 
 <template>
-   <ul class="project-navs" :class="{ open: isNavOpen, closed: !isNavOpen }">
-      <li class="project-navs__item">
-         <input type="checkbox" id="next" value="next" v-model="model" />
-         <label for="next"><NextJsIcon /> Next</label>
-      </li>
-      <li class="project-navs__item">
-         <input type="checkbox" id="react" value="react" v-model="model" />
-         <label for="react"><RiReactjsFill /> React</label>
-      </li>
-      <li class="project-navs__item">
-         <input type="checkbox" id="nuxt" value="nuxt" v-model="model" />
-         <label for="nuxt"><NuxtIcon /> Nuxt</label>
-      </li>
-      <li class="project-navs__item">
-         <input type="checkbox" id="vue" value="vue" v-model="model" />
-         <label for="vue"><RiVuejsFill /> Vue</label>
-      </li>
-      <li class="project-navs__item">
-         <input type="checkbox" id="html" value="html" v-model="model" />
-         <label for="html"><RiHtml5Fill /> HTML</label>
-      </li>
-      <li class="project-navs__item">
-         <input type="checkbox" id="css" value="css" v-model="model" />
-         <label for="css"><RiCss3Fill /> CSS</label>
-      </li>
-      <li class="project-navs__item">
-         <input type="checkbox" id="framer" value="framer" v-model="model" />
-         <label for="framer"><FramerIcon /> Framer</label>
-      </li>
-      <li class="project-navs__item">
-         <input type="checkbox" id="mui" value="mui" v-model="model" />
-         <label for="mui"><MuiIcon /> MUI</label>
-      </li>
-      <li class="project-navs__item">
-         <input
-            type="checkbox"
-            id="bootstrap"
-            value="bootstrap"
-            v-model="model"
-         />
-         <label for="bootstrap"><RiBootstrapFill /> Bootstrap</label>
-      </li>
-   </ul>
+   <div class="project-menu-wrapper" :class="{ open: isNavOpen }">
+      <ul class="project-navs">
+         <li class="project-navs__item">
+            <input type="checkbox" id="next" value="next" v-model="model" />
+            <label for="next"><NextJsIcon /> Next</label>
+         </li>
+         <li class="project-navs__item">
+            <input type="checkbox" id="react" value="react" v-model="model" />
+            <label for="react"><RiReactjsFill /> React</label>
+         </li>
+         <li class="project-navs__item">
+            <input type="checkbox" id="nuxt" value="nuxt" v-model="model" />
+            <label for="nuxt"><NuxtIcon /> Nuxt</label>
+         </li>
+         <li class="project-navs__item">
+            <input type="checkbox" id="vue" value="vue" v-model="model" />
+            <label for="vue"><RiVuejsFill /> Vue</label>
+         </li>
+         <li class="project-navs__item">
+            <input type="checkbox" id="html" value="html" v-model="model" />
+            <label for="html"><RiHtml5Fill /> HTML</label>
+         </li>
+         <li class="project-navs__item">
+            <input type="checkbox" id="css" value="css" v-model="model" />
+            <label for="css"><RiCss3Fill /> CSS</label>
+         </li>
+         <li class="project-navs__item">
+            <input type="checkbox" id="framer" value="framer" v-model="model" />
+            <label for="framer"><FramerIcon /> Framer</label>
+         </li>
+         <li class="project-navs__item">
+            <input type="checkbox" id="mui" value="mui" v-model="model" />
+            <label for="mui"><MuiIcon /> MUI</label>
+         </li>
+         <li class="project-navs__item">
+            <input
+               type="checkbox"
+               id="bootstrap"
+               value="bootstrap"
+               v-model="model"
+            />
+            <label for="bootstrap"><RiBootstrapFill /> Bootstrap</label>
+         </li>
+      </ul>
+   </div>
 </template>
 
 <style scoped lang="scss">
+.project-menu-wrapper {
+   width: 100%;
+}
 .project-navs {
    display: flex;
    flex-direction: column;
@@ -112,18 +117,22 @@ defineProps<Props>();
    }
 }
 @include media-query("max-width", "540px") {
-   .project-navs {
+   .project-menu-wrapper {
       width: 100%;
-      height: 0px;
-      opacity: 0;
-      padding-top: 15px;
-      transition: all 0.3s linear;
-      pointer-events: none;
+      display: grid;
+      grid-template-rows: 0fr;
+      transition: grid-template-rows 0.3s linear;
    }
-   .project-navs.open {
-      height: 375px;
-      opacity: 1;
-      pointer-events: auto;
+   .project-menu-wrapper.open {
+      grid-template-rows: 1fr;
+      & .project-navs {
+         padding: 15px 0px;
+      }
+   }
+   .project-navs {
+      padding: 0px;
+      overflow: hidden;
+      transition: padding 0.3s linear;
    }
 }
 </style>
