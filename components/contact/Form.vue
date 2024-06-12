084f1d0927f9f2c4f82ed1628ea6810f0994c56a
SKYE <skye@MacBook-Pro-MacBook.local>
2024-06-12 15:50:11 +0300

    #

--- components/contact/Form.vue
+++ components/contact/Form.vue
@@ -14,11 +14,26 @@ const formInputs = ref({
    email: "",
    message: ""
 });
+const formErrors = ref<String[]>();
+function formValidation() {
+   formErrors.value = [];
+   const { name, email, message } = formInputs.value;
+   if (name && email && message) {
+      return true;
+   } else {
+      !name && formErrors.value.push("name");
+      !email && formErrors.value.push("email");
+      !message && formErrors.value.push("message");
+      return false;
+   }
+}
+
 const isSendingMessage = ref(false);
 const pending = ref(false);
 const showMessageState = ref<"open" | "close" | null>(null);
 const responseMessage = ref("");
 function sumbit() {
+   if (!formValidation()) return;
    isSendingMessage.value = true;
    pending.value = true;
    $fetch<Response>(config.public.mailApi, {
@@ -50,15 +65,24 @@ function sumbit() {
 </script>
 <template>
    <form class="contact-form" @submit.prevent="sumbit">
-      <div class="contact-form__input">
+      <div
+         class="contact-form__input"
+         :class="{ error: formErrors?.includes('name') }"
+      >
          <label for="name">_name:</label>
          <input type="text" name="name" v-model="formInputs.name" />
       </div>
-      <div class="contact-form__input">
+      <div
+         class="contact-form__input"
+         :class="{ error: formErrors?.includes('email') }"
+      >
          <label for="email">_email:</label>
          <input type="email" name="email" v-model="formInputs.email" />
       </div>
-      <div class="contact-form__input">
+      <div
+         class="contact-form__input"
+         :class="{ error: formErrors?.includes('message') }"
+      >
          <label for="message">_message:</label>
          <textarea name="message" v-model="formInputs.message"></textarea>
       </div>
