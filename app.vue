5204be1f05c49b8af71d57749365a0ee3938da10
SKYE <skye@MacBook-Pro-MacBook.local>
2024-06-14 13:09:22 +0300

    Added keywords

--- app.vue
+++ app.vue
@@ -5,6 +5,11 @@ useHead({
       {
          name: "description",
          content: "This is the portfolio page for azeez abiola"
+      },
+      {
+         name: "keywords",
+         content:
+            "develop website, frontend developer, react developer, next js developer, web development, front end developer, web design company, web application development, freelance web developer, front end web developer, web development services"
       }
    ]
 });
